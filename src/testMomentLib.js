78e6d65e88b5bba9251ee5a9e92b1f110720c5dd
jungle0127 <arbor@foxmail.com>
2017-06-21 00:14:41 +0800

    added moment lib test script

---
+++ src/testMomentLib.js
@@ -0,0 +1,13 @@
+/**
+ * http://usejsdoc.org/
+ * http://momentjs.cn/
+ */
+var moment = require('moment');
+console.log(moment().locale('zh-CN').format('L'));
+console.log(moment().locale('zh-CN').format('l'));
+console.log(moment().locale('zh-CN').format('LL'));
+console.log(moment().locale('zh-CN').format('ll'));
+console.log(moment().locale('zh-CN').format('LLL'));
+console.log(moment().locale('zh-CN').format('lll'));
+console.log(moment().locale('zh-CN').format('LLLL'));
+console.log(moment().locale('zh-CN').format('llll'));
